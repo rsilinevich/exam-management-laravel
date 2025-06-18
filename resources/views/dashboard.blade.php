f89fd7a899de01b254b34bac6cd1ce3b5d4bd97c
Raimonds Siliņevičs <rsilinevich@gmail.com>
2025-06-18 17:15:48 +0300

    3rd commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,10 @@
                     <h1 class="text-2xl font-bold mb-6 text-center">Spring Session 2025 Exam Schedule Manager</h1>
 
                     <ul class="list-disc list-inside mb-4 text-gray-700">
-                        <li>You can add and edit exams.</li>
-                        <li>Admins can also delete exams.</li>
-                        <li>Use the navigation bar above to get started.</li>
+                        <li>2 lomas: “Admins” un parastais “Lietotājs”.</li>
+                        <li>Abi var pievienot un rediģēt eksāmenus.</li>
+                        <li>Tikai admins var arī dzēst eksāmenus.</li>
+                        <li>Lai sāktu, izmantojiet nav bar augšā. &#x2B06;</li>
                     </ul>
                 </div>
             </div>
